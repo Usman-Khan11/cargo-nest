5bcd91fcb4d3c0b249d23e031f7d73b9d436ecea
Muhammad Usman <usmanazhar357@gmail.com>
2025-01-27 17:13:07 +0500

    commit on 01-27-2025

--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -1,67 +1,83 @@
 @extends('admin.layouts.app')
 
+@section('top_nav_panel')
+    @if (request()->routeIs('admin.dashboard'))
+        <ul class="nav nav-tabs" id="myTabs" role="tablist"></ul>
+    @endif
+@endsection
+
 @section('panel')
+    @if (request()->routeIs('admin.dashboard'))
+        <style>
+            .layout-page {
+                padding-top: 15px !important;
+            }
+        </style>
+        <div>
+            <div class="tab-content bg-transparent p-0" id="myTabsContent"></div>
+        </div>
+    @endif
     <!--<div class="container-xxl flex-grow-1 container-p-y">-->
     <!--    <div class="row">-->
     <!--        <div class="col-lg-6 mb-4">-->
     <!--        </div>-->
     <!--    </div>-->
     <!--</div>-->
-    
-    <div class="content-wrapper">
+
+    <div class="content-wrapper d-none">
         <div class="container-xxl flex-grow-1 container-p-y">
             <div class="row">
 
                 <!-- Total Profit -->
                 <div class="col-xl-3 col-md-4 col-6 mb-4">
-                  <div class="card">
-                    <div class="card-body">
-                      <div class="badge p-2 bg-label-danger mb-2 rounded">
-                        <i class="ti ti-file-export ti-md"></i>
-                      </div>
-                      <h5 class="card-title mb-1 pt-2">Se Export</h5>
-                      <div class="pt-1">
-                        <a href="{{route('admin.quotation.create')}}"><span class="badge bg-label-secondary">View Now</span></a> 
-                      </div>
+                    <div class="card">
+                        <div class="card-body">
+                            <div class="badge p-2 bg-label-danger mb-2 rounded">
+                                <i class="ti ti-file-export ti-md"></i>
+                            </div>
+                            <h5 class="card-title mb-1 pt-2">Se Export</h5>
+                            <div class="pt-1">
+                                <a href="{{ route('admin.quotation.create') }}"><span class="badge bg-label-secondary">View
+                                        Now</span></a>
+                            </div>
+                        </div>
                     </div>
-                  </div>
                 </div>
 
                 <!-- Total Sales -->
                 <div class="col-xl-3 col-md-4 col-6 mb-4">
-                  <div class="card">
-                    <div class="card-body">
-                      <div class="badge p-2 bg-label-info mb-2 rounded"><i class="ti ti-file-import ti-md"></i></div>
-                      <h5 class="card-title mb-1 pt-2">Se Import</h5>
-                      <div class="pt-1">
-                        <a href=""><span class="badge bg-label-secondary">View Now</span></a>
-                      </div>
+                    <div class="card">
+                        <div class="card-body">
+                            <div class="badge p-2 bg-label-info mb-2 rounded"><i class="ti ti-file-import ti-md"></i></div>
+                            <h5 class="card-title mb-1 pt-2">Se Import</h5>
+                            <div class="pt-1">
+                                <a href=""><span class="badge bg-label-secondary">View Now</span></a>
+                            </div>
+                        </div>
                     </div>
-                  </div>
                 </div>
 
                 <!-- Revenue Growth -->
                 <div class="col-xl-4 col-md-8 mb-4">
-                  <div class="card">
-                    <div class="card-body">
-                      <div class="d-flex justify-content-between">
-                        <div class="d-flex flex-column">
-                          <div class="card-title mb-auto">
-                            <h5 class="mb-1 text-nowrap">Revenue Growth</h5>
-                            <small>Weekly Report</small>
-                          </div>
-                          <div class="chart-statistics">
-                            <h3 class="card-title mb-1">$4,673</h3>
-                            <span class="badge bg-label-success">+15.2%</span>
-                          </div>
+                    <div class="card">
+                        <div class="card-body">
+                            <div class="d-flex justify-content-between">
+                                <div class="d-flex flex-column">
+                                    <div class="card-title mb-auto">
+                                        <h5 class="mb-1 text-nowrap">Revenue Growth</h5>
+                                        <small>Weekly Report</small>
+                                    </div>
+                                    <div class="chart-statistics">
+                                        <h3 class="card-title mb-1">$4,673</h3>
+                                        <span class="badge bg-label-success">+15.2%</span>
+                                    </div>
+                                </div>
+                                <div id="revenueGrowth"></div>
+                            </div>
                         </div>
-                        <div id="revenueGrowth"></div>
-                      </div>
                     </div>
-                  </div>
                 </div>
-            </div>    
+            </div>
         </div>
     </div>
-    
 @endsection
--- resources/views/admin/layouts/master.blade.php
+++ resources/views/admin/layouts/master.blade.php
@@ -1,21 +1,20 @@
 <!DOCTYPE html>
-<html
-  lang="en"
-  class="light-style layout-navbar-fixed layout-menu-fixed"
-  dir="ltr"
-  data-theme="theme-default"
-  data-assets-path="{{ asset('assets') }}/"
-  data-template="vertical-menu-template-no-customizer">
-  <head>
+<html lang="en" class="light-style layout-navbar-fixed layout-menu-fixed" dir="ltr" data-theme="theme-default"
+    data-assets-path="{{ asset('assets') }}/" data-template="vertical-menu-template-no-customizer">
+
+<head>
     <meta charset="utf-8" />
-    <meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=no, minimum-scale=1.0, maximum-scale=1.0" />
+    <meta name="viewport"
+        content="width=device-width, initial-scale=1.0, user-scalable=no, minimum-scale=1.0, maximum-scale=1.0" />
     <meta name="description" content="{{ $seo_desc }}">
     <meta name="keywords" content="{{ $seo_keywords }}">
     <title>{{ $seo_title }} | {{ App\Models\GeneralSetting::first()->sitename }}</title>
     <link rel="icon" type="image/x-icon" href="{{ asset('assets/img/favicon/favicon-ico.png') }}" />
     <link rel="preconnect" href="https://fonts.googleapis.com" />
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
-    <link href="https://fonts.googleapis.com/css2?family=Public+Sans:ital,wght@0,300;0,400;0,500;0,600;0,700;1,300;1,400;1,500;1,600;1,700&display=swap" rel="stylesheet" />
+    <link
+        href="https://fonts.googleapis.com/css2?family=Public+Sans:ital,wght@0,300;0,400;0,500;0,600;0,700;1,300;1,400;1,500;1,600;1,700&display=swap"
+        rel="stylesheet" />
     <link rel="stylesheet" href="{{ asset('assets/vendor/fonts/fontawesome.css') }}" />
     <link rel="stylesheet" href="{{ asset('assets/vendor/fonts/tabler-icons.css') }}" />
     <link rel="stylesheet" href="{{ asset('assets/vendor/fonts/flag-icons.css') }}" />
@@ -29,19 +28,38 @@
     <link rel="stylesheet" href="{{ asset('assets/vendor/libs/apex-charts/apex-charts.css') }}" />
     <link rel="stylesheet" href="{{ asset('assets/vendor/libs/swiper/swiper.css') }}" />
     <link rel="stylesheet" href="{{ asset('assets/vendor/libs/datatables-bs5/datatables.bootstrap5.css') }}" />
-    <link rel="stylesheet" href="{{ asset('assets/vendor/libs/datatables-responsive-bs5/responsive.bootstrap5.css') }}" />
-    <link rel="stylesheet" href="{{ asset('assets/vendor/libs/datatables-checkboxes-jquery/datatables.checkboxes.css') }}" />
+    <link rel="stylesheet"
+        href="{{ asset('assets/vendor/libs/datatables-responsive-bs5/responsive.bootstrap5.css') }}" />
+    <link rel="stylesheet"
+        href="{{ asset('assets/vendor/libs/datatables-checkboxes-jquery/datatables.checkboxes.css') }}" />
     <link rel="stylesheet" href="{{ asset('assets/vendor/css/pages/cards-advance.css') }}" />
     <link rel="stylesheet" href="{{ asset('assets/vendor/libs/select2/select2.css') }}" />
     <link rel="stylesheet" href="{{ asset('assets/vendor/libs/bootstrap-select/bootstrap-select.css') }}" />
     <script src="{{ asset('assets/vendor/js/helpers.js') }}"></script>
     <script src="{{ asset('assets/js/config.js') }}"></script>
     @yield('style')
-  </head>
+</head>
 
-  <body>
-     @yield('content')
-    
+@if (!request()->routeIs('admin.dashboard'))
+    <style>
+        #layout-menu {
+            display: none !important;
+        }
+
+        .layout-page {
+            padding-left: 0 !important;
+        }
+
+        .layout-navbar-fixed .layout-navbar.navbar-detached {
+            width: 100% !important;
+            left: 1.25rem !important;
+        }
+    </style>
+@endif
+
+<body>
+    @yield('content')
+
     <script>
         function checkDelete() {
             return confirm('Are you sure?');
@@ -66,85 +84,190 @@
     <script src="{{ asset('assets/js/dashboards-analytics.js') }}"></script>
     @stack('script')
     @include('admin.partials.notify')
-    
+
     <script>
-        $('#navbar-search__field').on('input', function () {
+        $('#navbar-search__field').on('input', function() {
             var search = $(this).val().toLowerCase();
             var search_result_pane = $('.navbar_search_result');
             $(search_result_pane).html('');
             if (search.length == 0) {
                 $('#navbar_search_result_area').css('display', 'none');
-              return;
+                return;
             }
             // search
-            var match = $('.menu-inner .menu-link').filter(function (idx, elem) {
-              return $(elem).text().trim().toLowerCase().indexOf(search) >= 0 ? elem : null;
+            var match = $('.menu-inner .menu-link').filter(function(idx, elem) {
+                return $(elem).text().trim().toLowerCase().indexOf(search) >= 0 ? elem : null;
             }).sort();
-        
+
             // show search result
             // search not found
             if (match.length == 0) {
-              $(search_result_pane).append('<li class="text-muted pl-5">No search result found.</li>');
-              return;
+                $(search_result_pane).append('<li class="text-muted pl-5">No search result found.</li>');
+                return;
             }
             // search found
-            match.each(function (idx, elem) {
-              var item_url = $(elem).attr('href') || $(elem).data('default-url');
-              var item_text = $(elem).text().replace(/(\d+)/g, '').trim();
-              $(search_result_pane).append(`<li><a href="${item_url}">${item_text}</a></li>`);
+            match.each(function(idx, elem) {
+                var item_url = $(elem).attr('href') || $(elem).data('default-url');
+                var item_text = $(elem).text().replace(/(\d+)/g, '').trim();
+                $(search_result_pane).append(
+                    `<li><a class="nav_link" href="${item_url}">${item_text}</a></li>`);
             });
-            
+
             $('#navbar_search_result_area').css('display', 'block');
-          });
-          
-          
-         function getList(route, type, id) {
-          let data = null;
-          $.ajax({
-            url: route,
-            type: "POST",
-            data: {
-              id: id,
-              type: type,
-              _token: "{{ csrf_token() }}",
-            },
-            async: false,
-            success: function (res) {
-              if (res) {
-                data = res;
-              }
-            },
-          });
-          return data;
+        });
+
+
+        function getList(route, type, id) {
+            let data = null;
+            $.ajax({
+                url: route,
+                type: "POST",
+                data: {
+                    id: id,
+                    type: type,
+                    _token: "{{ csrf_token() }}",
+                },
+                async: false,
+                success: function(res) {
+                    if (res) {
+                        data = res;
+                    }
+                },
+            });
+            return data;
         }
-        
+
         function deleteData(route) {
             let c = confirm('Are you sure?');
             let id = parseInt($("#myForm input[name=id]").val());
-            if(id > 0 && c && route){
+            if (id > 0 && c && route) {
                 window.location.assign(route + '/' + id);
-            } else if(!id || !route) {
-                iziToast.error({ message: 'Something went wrong!', position: "topRight" });
+            } else if (!id || !route) {
+                iziToast.error({
+                    message: 'Something went wrong!',
+                    position: "topRight"
+                });
             }
         }
-        
+
         function formReset(route) {
             document.getElementById('myForm').reset();
             $("#myForm").attr('action', route);
             $("#myForm").find("select option:first").attr("selected", true);
-            $("#myForm").find("select").trigger("change"); 
+            $("#myForm").find("select").trigger("change");
         }
-        
+
         function initializeSelect2(elem) {
             let arr = elem.join(', ');
             $(arr).select2();
         }
-        
-        $(document).ready(function(){
-            setTimeout(function() { 
+
+        $(document).ready(function() {
+            let iframe_height = $(window).height() - 100;
+
+            let storedWindows = JSON.parse(localStorage.getItem("openedWindows")) || {};
+            let storedWindowsLength = Object.keys(storedWindows).length;
+
+            setTimeout(function() {
                 //$(".navigation[data-type=last]").click();
+                for (const id in storedWindows) {
+                    const url = storedWindows[id].url;
+                    const name = storedWindows[id].name;
+                    const focus = storedWindows[id].focus;
+
+                    $('#myTabs').append(addTabList(id, name));
+                    $('#myTabsContent').append(addTabContent(id, url, iframe_height));
+
+                    if (focus) {
+                        $(`#myTabs [data-bs-target="#${id}"]`).tab("show");
+                    }
+                }
             }, 500);
+
+            $(document).on("click", ".nav_link, .menu-inner .menu-link", function(event) {
+                event.preventDefault();
+
+                if ($(this).hasClass('menu-toggle')) {
+                    return;
+                }
+
+                let name = $(this).text().trim();
+                let url = $(this).attr('href');
+
+                if (name == "Logout") {
+                    localStorage.setItem("openedWindows", JSON.stringify({}));
+                    window.location.assign(url)
+                    return;
+                }
+
+                if (name && url) {
+                    let id = 't_' + slugify(name);
+
+                    // focus false of all tabs
+                    Object.keys(storedWindows).forEach(key => (storedWindows[key].focus = false));
+
+                    if (!$(`#myTabs [data-bs-target="#${id}"]`).length) {
+
+                        if (storedWindowsLength >= 10) {
+                            notify('error', 'Tab Length Exceed!')
+                            return;
+                        }
+
+                        const tabContent = addTabContent(id, url, iframe_height);
+
+                        $('#myTabs').append(addTabList(id, name));
+                        $('#myTabsContent').append(tabContent);
+                        $(`#myTabs [data-bs-target="#${id}"]`).tab("show");
+
+                        storedWindows[id] = {
+                            name: name,
+                            url: url,
+                            focus: true
+                        };
+
+                        storedWindowsLength++;
+                    } else {
+                        // focus true of this tab
+                        Object.keys(storedWindows).forEach(key => (storedWindows[key].focus = key === id));
+                        $(`#myTabs [data-bs-target="#${id}"]`).tab("show");
+                    }
+
+                    // Update localStorage with metadata
+                    localStorage.setItem("openedWindows", JSON.stringify(storedWindows));
+                }
+            })
+
+            // focus true on tab click
+            $('#myTabs [data-bs-toggle="tab"]').on('shown.bs.tab', function(e) {
+                let id = $(this).data("id");
+                Object.keys(storedWindows).forEach(key => (storedWindows[key].focus = key === id));
+                localStorage.setItem("openedWindows", JSON.stringify(storedWindows));
+            });
         })
+
+        function addTabList(id, name) {
+            return `<li class="nav-item" role="presentation">
+                        <button class="nav-link" data-bs-toggle="tab" data-id="${id}" data-bs-target="#${id}"
+                            type="button" role="tab">${name}</button>
+                    </li>`;
+        }
+
+        function addTabContent(id, url, height) {
+            return `<div class="tab-pane fade" id="${id}" role="tabpanel">
+                      <iframe src="${url}" style="width: 100%; height: ${height}px; border: none;"></iframe>
+                    </div>`;
+        }
+
+        function slugify(text) {
+            return text
+                .toString() // Convert to string
+                .trim() // Remove leading/trailing whitespace
+                .toLowerCase() // Convert to lowercase
+                .replace(/<\/?[^>]+(>|$)/g, "") // Remove HTML tags if present
+                .replace(/[\s\W-]+/g, "-") // Replace spaces and non-alphanumeric characters with a hyphen
+                .replace(/^-+|-+$/g, ""); // Remove leading/trailing hyphens
+        }
     </script>
-  </body>
+</body>
+
 </html>
